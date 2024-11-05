e77cb37477a73a488395013c134eb5f57702119c
JunhyunKang <kangjunhyun38@gmail.com>
2024-11-05 17:12:50 +0900

    chore: session, cookies-parser추가
    Task: #4

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+// somewhere in your initialization file
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.use(cookieParser());
+  app.use(
+      session({
+        secret: 'my-secret',
+        resave: false,
+        saveUninitialized: false,
+      }),
+  );
 }
 bootstrap();
