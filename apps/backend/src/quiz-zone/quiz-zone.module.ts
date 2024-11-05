4fed30799e8a00eb82056c0077f2ced519ada6af
mario <codemario318@gmail.com>
2024-11-05 18:21:31 +0900

    chore: backend nest 환경설정 관련
    - nest 버전 동기화
    
    Story: #4

--- apps/backend/src/quiz-zone/quiz-zone.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { QuizZoneService } from './quiz-zone.service';
-import { QuizZoneController } from './quiz-zone.controller';
-
-@Module({
-  controllers: [QuizZoneController],
-  providers: [QuizZoneService],
-})
-export class QuizZoneModule {}
