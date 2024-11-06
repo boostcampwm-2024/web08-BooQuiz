75a394eeb665a543d2ea375e1c9f08e8ad9a4be3
JunhyunKang <kangjunhyun38@gmail.com>
2024-11-06 16:45:30 +0900

    test: quiz-zone.service 테스트 코드 작성
    - quiz-zone.service.spec.ts에 사용자sid기반으로 퀴즈존 초기화 테스트 코드
    
    Story: #3
    Task: #4

--- apps/backend/src/quiz-zone/quiz-zone.service.spec.ts
+++ apps/backend/src/quiz-zone/quiz-zone.service.spec.ts
@@ -1,33 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { QuizZoneService } from './quiz-zone.service';
+import { quizzes, QuizZoneService } from './quiz-zone.service';
+import { QuizZoneRepositoryMemory } from './quiz-zone.repository.memory';
+import { QuizZone } from './entities/quiz-zone.entity';
+import { SubmittedQuiz } from './entities/submitted.quiz';
 
 describe('QuizZoneService', () => {
-  let service: QuizZoneService;
+    let service: QuizZoneService;
+    let repository: QuizZoneRepositoryMemory;
+    let data: Record<string, QuizZone>;
+    beforeEach(async () => {
+        data = {};
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [QuizZoneService],
-    }).compile();
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                QuizZoneService,
+                QuizZoneRepositoryMemory,
+                {
+                    provide: 'DATA',
+                    useValue: data,
+                },
+            ],
+        }).compile();
 
-    service = module.get<QuizZoneService>(QuizZoneService);
-  });
-
-  describe('사용자가 퀴즈존 생성 요청을 보내면 퀴즈존을 생성한다.', () => {
-    it('서버는 사용자의 세션 ID를 이용하여 퀴즈존을 생성한다.', () => {
-      const sid = '1234';
-      service.create(sid);
+        service = module.get<QuizZoneService>(QuizZoneService);
+        repository = module.get<QuizZoneRepositoryMemory>(QuizZoneRepositoryMemory);
     });
 
-    it('사용자의 세션 ID로 퀴즈존 정보를 초기화한다.', () => {
+    describe('사용자가 퀴즈존 생성 요청을 보내면 퀴즈존을 생성한다.', () => {
+        it('서버는 사용자의 세션 ID를 이용하여 퀴즈존을 생성한다.', () => {
+            const sid = '1234';
 
+            service.create(sid);
+            expect(data.hasOwnProperty(sid)).toEqual(true);
+        });
+
+        it('퀴즈존은 입장한 사용자 정보로 초기화된다.', () => {
+            const sid = '1234';
+
+            service.create(sid);
+            expect(data[sid].player.id).toEqual(sid);
+        });
+        it('퀴즈존이 초기화되면 현재퀴즈번호는 0이다.', () => {
+            const sid = '1234';
+
+            service.create(sid);
+            expect(data[sid].currentQuizIndex).toEqual(0);
+        });
+        it('퀴즈존이 초기화되면 문제들이 할당된다.', () => {
+            const sid = '1234';
+            service.create(sid);
+            expect(data[sid].quizzes).toBe(quizzes);
+        });
+        it('퀴즈존이 초기화되면 로비상태가 된다.', () => {
+            const sid = '1234';
+
+            service.create(sid);
+            expect(data[sid].stage).toEqual('LOBBY');
+        });
     });
 
-    it('퀴즈셋을 퀴즈존 세션에 저장한다.', () => {
-
+    it('should be defined', () => {
+        expect(service).toBeDefined();
     });
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
 });
--- apps/backend/src/quiz-zone/quiz-zone.service.ts
+++ apps/backend/src/quiz-zone/quiz-zone.service.ts
@@ -3,30 +3,34 @@ import { CreateQuizZoneDto } from './dto/create-quiz-zone.dto';
 import { UpdateQuizZoneDto } from './dto/update-quiz-zone.dto';
 import { QuizZoneRepositoryInterface } from './quiz-zone.repository.interface';
 import { QuizZoneRepositoryMemory } from './quiz-zone.repository.memory';
+import { Quiz } from './entities/quiz.entity';
 
-@Injectable()
+export const quizzes: Quiz[] = [
+    { index: 0, question: 'What is the capital of Korea?', answer: 'Seoul' },
+    { index: 1, question: 'WsadfsafKorea?', answer: 'sadfsaf' },
+    { index: 2, question: 'Wsdfaf?', answer: 'cvxvcx' },
+];
+
 export class QuizZoneService {
-  constructor(
-    private readonly quizZoneRepository: QuizZoneRepositoryMemory
-  ) {}
+    constructor(private readonly quizZoneRepository: QuizZoneRepositoryMemory) {}
 
-  create(sessionId: string) {
-    return 'This action adds a new quizZone';
-  }
+    create(sessionId: string) {
+        return 'This action adds a new quizZone';
+    }
 
-  findAll() {
-    return `This action returns all quizZone`;
-  }
+    findAll() {
+        return `This action returns all quizZone`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} quizZone`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} quizZone`;
+    }
 
-  update(id: number, updateQuizZoneDto: UpdateQuizZoneDto) {
-    return `This action updates a #${id} quizZone`;
-  }
+    update(id: number, updateQuizZoneDto: UpdateQuizZoneDto) {
+        return `This action updates a #${id} quizZone`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} quizZone`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} quizZone`;
+    }
 }
