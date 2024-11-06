45324eb6622a84ca85095859fb042a7762972558
krokerdile <crazykroker@gmail.com>
2024-11-06 19:13:15 +0900

    feat: 레이아웃 컴포넌트 스타일 수정
    - RootLayout 컴포넌트 스타일 수정
    
    Task: #35

--- apps/frontend/src/pages/RootLayout.tsx
+++ apps/frontend/src/pages/RootLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
-  return (
-    <div className="min-h-screen">
-      안녕하세요 레이아웃입니다.
-      <Outlet />
-    </div>
-  );
+    return (
+        <div className="min-h-screen min-w-full p-4">
+            안녕하세요 레이아웃입니다.
+            <Outlet />
+        </div>
+    );
 };
 
 export default RootLayout;
