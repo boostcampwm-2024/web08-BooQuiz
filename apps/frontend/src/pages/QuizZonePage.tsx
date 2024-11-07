21c00cd82a8fd270adfda28eefde62ac5568d1b9
krokerdile <crazykroker@gmail.com>
2024-11-08 00:57:45 +0900

    feat: 퀴즈 풀이 페이지 구현
    - QuizZonePage로 임시 명명하여 퀴즈 풀이 페이지 구현
    - 차후에 퍼널로 수정 예정
    - 나가기 버튼을 누르면 결과 페이지로 이동 할 수 있도록 추가
    
    Task: 퀴즈 풀이 페이지로 넘어온 사용자는 풀어야 할 문제를 확인할 수 있다. #8

--- apps/frontend/src/pages/QuizZonePage.tsx
+++ apps/frontend/src/pages/QuizZonePage.tsx
@@ -1,5 +1,41 @@
+import CommonButton from '@/components/common/CommonButton';
+import ContentBox from '@/components/common/ContentBox';
+import Input from '@/components/common/Input';
+import ProgressBar from '@/components/common/ProgressBar';
+import Typography from '@/components/common/Typogrpahy';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const QuizZonePage = () => {
-    return <></>;
+    const [answer, setAnswer] = useState('');
+    const [question] = useState('저희 팀의 이름은 무엇인가요?');
+    const navigate = useNavigate();
+
+    const handleMoveToWaiting = () => {
+        navigate('/waiting');
+    };
+
+    const handleMoveToResult = () => {
+        navigate('/result');
+    };
+
+    return (
+        <>
+            <img width="200px" src="/BooQuizLogo.png" alt="BooQuiz Logo" />
+            <ProgressBar maxTime={10} onTimeEnd={handleMoveToWaiting} />
+            <ContentBox>
+                <Typography text={`Q. ${question}`} size="2xl" color="black" bold={true} />
+                <Input
+                    onChange={(e) => setAnswer(e.target.value)}
+                    name="quizAnswer"
+                    value={answer}
+                    placeholder={'정답을 입력해주세요'}
+                />
+                <CommonButton text={'제출하기'} clickEvent={handleMoveToWaiting} />
+                <CommonButton text={'나가기'} clickEvent={handleMoveToResult} />
+            </ContentBox>
+        </>
+    );
 };
 
 export default QuizZonePage;
