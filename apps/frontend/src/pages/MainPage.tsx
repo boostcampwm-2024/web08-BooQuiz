78c2d279d0584e1ee63e09c10e570b216b0782b6
krokerdile <crazykroker@gmail.com>
2024-11-08 00:22:14 +0900

    feat: 메인페이지 구현
    - 공통 컴포넌트 활용하여 MainPage 구현
    - 서버 연결 부는 차후에 고려하고 일단 페이지 router 적용
    
    Task: #2

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,20 +1,40 @@
 import CommonButton from '@/components/common/CommonButton';
-import { Button } from '@/components/ui/button';
+import ContentBox from '@/components/common/ContentBox';
+import Typography from '@/components/common/Typogrpahy';
 import { useNavigate } from 'react-router-dom';
 
 const MainPage = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleMoveToMain = () => {
-    navigate('/waiting');
-  };
+    const handleMoveToMain = () => {
+        navigate('/waiting');
+    };
 
-  return (
-    <>
-      <Button onClick={handleMoveToMain}>퀴즈존 참여하기</Button>
-      <CommonButton text="퀴즈존 참여하기" isFulfill={true} clickEvent={void handleMoveToMain} />
-    </>
-  );
+    return (
+        <>
+            <img src="/BooQuizLogo.png" alt="BooQuiz Logo" />
+            <Typography
+                size="4xl"
+                color="blue"
+                text="시작하기 버튼을 눌러 퀴즈를 시작해보세요"
+                bold={true}
+            />
+            <ContentBox>
+                <Typography size="base" color="blue" text="퀴즈 참여하기" bold={true} />
+                <Typography
+                    size="xs"
+                    color="gray"
+                    text="버튼을 눌러 퀴즈존에 참여해보세요"
+                    bold={true}
+                />
+                <CommonButton
+                    text="퀴즈존 참여하기"
+                    isFulfill={true}
+                    clickEvent={handleMoveToMain}
+                />
+            </ContentBox>
+        </>
+    );
 };
 
 export default MainPage;
