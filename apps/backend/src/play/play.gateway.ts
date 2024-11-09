76e561b17a150f850d7850f2a55ac7edc30e9c33
mario <codemario318@gmail.com>
2024-11-09 16:52:56 +0900

    test: superwstest 적용
    - WebSocket 테스트 모듈 superwstest 추가
    - WebSocket 테스트 코드 추가
    
    Task: #5

--- apps/backend/src/play/play.gateway.ts
+++ apps/backend/src/play/play.gateway.ts
@@ -1,27 +1,19 @@
-import {
-    MessageBody,
-    OnGatewayConnection,
-    SubscribeMessage,
-    WebSocketGateway,
-} from '@nestjs/websockets';
+import { OnGatewayConnection, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { PlayService } from './play.service';
-import { CreatePlayDto } from './dto/create-play.dto';
 import { WebSocket } from 'ws';
 import { IncomingMessage } from 'http';
 
-@WebSocketGateway(3000,{path:'/play'})
+@WebSocketGateway(3000, { path: '/play' })
 export class PlayGateway implements OnGatewayConnection {
     constructor(private readonly playService: PlayService) {}
 
     handleConnection(client: WebSocket, request: IncomingMessage) {
-        // console.log(client);
-        // console.log(request.headers);
-        // console.log(request.headers.cookie);
+        client.send('connected');
     }
 
     @SubscribeMessage('createPlay')
-    create(client:any,data: any) {
+    create(client: any, data: any) {
         // return this.playService.create(createPlayDto);
-        return { event: 'pong'};
+        return { event: 'pong' };
     }
 }
