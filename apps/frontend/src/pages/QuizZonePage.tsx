b5e36e66a2e2c80aca16517ebd1243dd04fa1b91
krokerdile <crazykroker@gmail.com>
2024-11-12 19:55:36 +0900

    feat: 퀴즈존 초기 데이터 로직 추가 및 웹소켓 연결 로직 구현
    - handleGetQuizZoneData()를 통해 퀴즈존 대기실 페이지 초기 데이타 받아오기
    - 웹소켓 생성 및 연결 로직 구현
    - 퀴즈 시작 요청 처리 구현
    
    Task: #2

--- apps/frontend/src/pages/QuizZonePage.tsx
+++ apps/frontend/src/pages/QuizZonePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useQuizZoneManager } from '../hook/useQuizZoneManager';
 import QuizZoneLobby from '@/blocks/QuizZone/QuizZoneLobby';
 import QuizWaiting from '@/blocks/QuizZone/QuizWaiting';
@@ -11,6 +11,63 @@ interface QuizZoneProps {
 }
 
 export const QuizZone = ({ pinNumber }: QuizZoneProps) => {
+    const [totalQuizCount, setTotalQuizCount] = useState(0);
+    const handleGetQuizZoneData = () => {
+        fetch(`http://localhost:3000/quiz-zone/${id}`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to fetch quiz waiting room');
+                }
+            })
+            .then((data) => {
+                console.log('data:', data);
+
+                setTotalQuizCount(data.quizCount);
+                console.log('totalQuizCount:', totalQuizCount);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
+    const [ws, setWs] = useState<WebSocket>();
+    //임시로 렌더링 시에 요청 하고 totalCount 적용
+    //로직 수정 필요
+    useEffect(() => {
+        handleGetQuizZoneData();
+        //소켓 연결
+        const ws = new WebSocket('ws://localhost:3000/play');
+        setWs(ws);
+
+        ws.onopen = () => {
+            console.log('연결됨');
+        };
+
+        // 서버로부터 메시지 수신
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            console.log('서버로부터 받은 데이터:', data);
+            // 서버로부터 받은 데이터에 따라 처리
+            // event를 기준으로 객체 형태 데이터가 서버로부터 전달됨
+            switch (data.event) {
+                case 'start':
+                    console.log(data.data);
+                    break;
+            }
+        };
+
+        ws.onclose = () => {
+            console.log('시스템', '서버와 연결이 끊어졌습니다.');
+        };
+    }, [totalQuizCount]);
+
     const {
         quizZone,
         solveStage,
@@ -23,16 +80,21 @@ export const QuizZone = ({ pinNumber }: QuizZoneProps) => {
         startQuiz,
         submitAnswer,
     } = useQuizZoneManager({
-        totalQuizzes: 2,
+        totalQuizzes: totalQuizCount,
         onQuizComplete: () => console.log('퀴즈 완료!'),
         onError: (error) => console.error('에러 발생:', error),
     });
 
+    const id = 1;
+
     useEffect(() => {
+        const Data = handleGetQuizZoneData();
+        console.log(Data);
         // 로비 초기 데이터 설정
         updateStageData('Lobby', {
             participants: 1,
             isHost: true,
+            totalQuizCount: totalQuizCount,
             quizTitle: '연습 퀴즈',
             description: '혼자서 풀어보는 연습 퀴즈입니다.',
         });
@@ -52,7 +114,17 @@ export const QuizZone = ({ pinNumber }: QuizZoneProps) => {
             totalParticipants: 1,
             correctAnswers: 2,
         });
-    }, []);
+    }, [totalQuizCount]);
+
+    const handleSocketQuizStart = () => {
+        console.log(typeof ws);
+        console.log(ws);
+        ws.send(
+            JSON.stringify({
+                event: 'start',
+            }),
+        );
+    };
 
     // 로비 화면 렌더링
     const renderLobby = () => (
@@ -61,7 +133,10 @@ export const QuizZone = ({ pinNumber }: QuizZoneProps) => {
                 <QuizZoneLobby
                     quizZoneData={quizZoneData}
                     pinNumber={pinNumber}
-                    startQuiz={startQuiz}
+                    startQuiz={() => {
+                        startQuiz();
+                        handleSocketQuizStart();
+                    }}
                 />
             )}
         </>
@@ -70,6 +145,7 @@ export const QuizZone = ({ pinNumber }: QuizZoneProps) => {
     // 퀴즈 진행 화면 렌더링
     const renderQuizProgress = () => {
         const currentQuiz = quizZoneData.quizProgress?.currentQuiz;
+        console.log(quizZoneData.quizProgress, currentQuiz);
         const isLastQuiz = quizProgress.currentQuizIndex === quizProgress.totalQuizzes - 1;
 
         if (solveStage === 'WAITING') {
