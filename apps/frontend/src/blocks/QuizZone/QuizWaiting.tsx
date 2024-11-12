07916f583bf5309f92965d8e5890b39d1b17f88b
krokerdile <crazykroker@gmail.com>
2024-11-12 16:47:33 +0900

    feat: 기존 퀴즈 풀이 대기 페이지 위에 퍼널 적용
    - 기존에 구현해둔 풀이 대기 페이지에 퍼널에서 제공하는 props 적용하여 구현
    
    Task: 퀴즈존 페이지 useFunnel 적용하기 #57

---
+++ apps/frontend/src/blocks/QuizZone/QuizWaiting.tsx
@@ -0,0 +1,29 @@
+import ContentBox from '@/components/common/ContentBox';
+import TimerDisplay from '@/components/common/TimerDisplay';
+import Typography from '@/components/common/Typogrpahy';
+
+interface QuizWaitingProps {
+    prepareTime: number | null;
+}
+
+const QuizWaiting = ({ prepareTime }: QuizWaitingProps) => {
+    return (
+        <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
+            <img width="200px" src="/BooQuizLogo.png" alt="BooQuiz Logo" />
+            <ContentBox>
+                <div className="w-full flex flex-col items-center gap-2">
+                    <Typography
+                        size="3xl"
+                        color="blue"
+                        text="다음 퀴즈를 준비 중입니다."
+                        bold={true}
+                    />
+                    <Typography size="3xl" color="blue" text="잠시만 기다려주세요" bold={true} />
+                </div>
+                <TimerDisplay time={prepareTime ?? 0} isFulfill={true} />
+            </ContentBox>
+        </div>
+    );
+};
+
+export default QuizWaiting;
