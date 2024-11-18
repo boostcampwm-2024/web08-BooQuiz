451d43deafdc6b22275ca6af1061f5cdd4e17636
mario <codemario318@gmail.com>
2024-11-18 18:24:54 +0900

    chore: winston 반영 및 예시 추가
    - winston 로거 추가
    - winston 기본 설정 추가
    - winston 사용 예시 추가
    
    Task: #94

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizZoneModule } from './quiz-zone/quiz-zone.module';
 import { PlayModule } from './play/play.module';
 import { ConfigModule } from '@nestjs/config';
 import httpConfig from '../config/http.config';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './logger/winston.config';
+import { HttpLoggingMiddleware } from './logger/http-logger.middleware';
 
 @Module({
     imports: [
@@ -13,8 +16,13 @@ import httpConfig from '../config/http.config';
         ConfigModule.forRoot({
             load: [httpConfig],
         }),
+        WinstonModule.forRoot(winstonConfig),
     ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(HttpLoggingMiddleware).forRoutes('*');
+    }
+}
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    constructor(@Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {}
+
+    getHello(): string {
+        this.logger.error('HELL');
+        return 'Hello World!';
+    }
 }
