c06cefb872b9b0700810e00e6aafcd3abfaf8b13
선빈 <ehtjsqls@gmail.com>
2024-11-19 15:19:00 +0900

    test: 레포지토리 테스트 작성
    Story: #86
    Task: #89

--- apps/backend/src/quiz-zone/repository/quiz-zone.memory.repository.spec.ts
+++ apps/backend/src/quiz-zone/repository/quiz-zone.memory.repository.spec.ts
@@ -25,38 +25,48 @@ describe('QuizZoneRepositoryMemory', () => {
 
     describe('set', () => {
         it('세션 id와 퀴즈존 정보를 통해 새로운 퀴즈존 정보를 저장한다.', async () => {
-            const id = 'some-id';
+            const quizZoneId = 'some-id';
+
             const quizZone: QuizZone = {
-                currentQuizDeadlineTime: 0,
+                players: new Map(),
+                adminId: 'adminId',
+                maxPlayers: 4,
+                quizzes: [],
+                stage: 'LOBBY',
+                title: 'title',
+                description: 'description',
                 currentQuizIndex: 0,
                 currentQuizStartTime: 0,
-                player: undefined,
-                quizzes: [],
-                stage: undefined,
+                currentQuizDeadlineTime: 0,
                 intervalTime: 30_000,
             };
 
-            await repository.set(id, quizZone);
+            await repository.set(quizZoneId, quizZone);
 
-            expect(storage.get(id)).toEqual(quizZone); // 생성된 객체와 동일한지 확인
+            expect(storage.get(quizZoneId)).toEqual(quizZone); // 생성된 객체와 동일한지 확인
         });
 
         //1인 사용자일 때는 접속 할 때마다 퀴즈존 초기화
-        it.skip('중복된 key 값이 입력으로 들어오면 에러가 발생한다.', async () => {
-            const id = 'some-id';
+        it('중복된 key 값이 입력으로 들어오면 에러가 발생한다.', async () => {
+            const quizZoneId = 'some-id';
+
             const quizZone: QuizZone = {
-                currentQuizDeadlineTime: 0,
+                players: new Map(),
+                adminId: 'adminId',
+                maxPlayers: 4,
+                quizzes: [],
+                stage: 'LOBBY',
+                title: 'title',
+                description: 'description',
                 currentQuizIndex: 0,
                 currentQuizStartTime: 0,
-                player: undefined,
-                quizzes: [],
-                stage: undefined,
+                currentQuizDeadlineTime: 0,
                 intervalTime: 30_000,
             };
 
-            await repository.set(id, quizZone);
+            await repository.set(quizZoneId, quizZone);
 
-            expect(repository.set(id, quizZone)).rejects.toThrow(ConflictException);
+            expect(repository.set(quizZoneId, quizZone)).rejects.toThrow(ConflictException);
         });
     });
 });
