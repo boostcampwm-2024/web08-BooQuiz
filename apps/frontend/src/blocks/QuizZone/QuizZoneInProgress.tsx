661bba2b86f1d372e63b9005db544eea18210e2a
krokerdile <crazykroker@gmail.com>
2024-11-21 07:31:39 +0900

    feat: 퀴즈 완료 상태 및 마지막 퀴즈 처리 로직 추가
    - QuizCompleted 컴포넌트 추가 및 SUBMIT 상태 처리
    - 마지막 퀴즈 여부 확인 로직 추가
    - currentQuiz 객체에 대한 optional chaining 적용
    - deadlineTime 변수 제거
    
    Task: #98

--- apps/frontend/src/blocks/QuizZone/QuizZoneInProgress.tsx
+++ apps/frontend/src/blocks/QuizZone/QuizZoneInProgress.tsx
@@ -1,6 +1,7 @@
 import { QuizZone } from '@/types/quizZone.types';
 import QuizWaiting from './QuizWaiting';
 import QuizInProgress from './QuizInProgress';
+import QuizCompleted from './QuizCompleted';
 
 interface QuizZoneInProgressProps {
     quizZoneState: QuizZone;
@@ -9,23 +10,26 @@ interface QuizZoneInProgressProps {
 }
 
 const QuizZoneInProgress = ({ quizZoneState, submitAnswer, playQuiz }: QuizZoneInProgressProps) => {
-    const { currentPlayer, currentQuiz } = quizZoneState;
+    const { currentPlayer, currentQuiz, stage } = quizZoneState;
     const { state } = currentPlayer;
-    const { playTime, startTime, deadlineTime } = currentQuiz;
+    const { playTime, startTime } = currentQuiz ?? {};
     console.log(startTime, state);
     switch (state) {
         case 'WAIT':
-            return <QuizWaiting startTime={startTime} playQuiz={playQuiz} />;
+            return <QuizWaiting startTime={startTime!} playQuiz={playQuiz} />;
         case 'PLAY':
             return (
                 <QuizInProgress
-                    playTime={playTime}
+                    playTime={playTime!}
                     currentQuiz={currentQuiz}
                     submitAnswer={submitAnswer}
                 />
             );
-        // case 'SUBMIT':
-        //     return <QuizWaiting />;
+        case 'SUBMIT':
+            const isLastQuiz =
+                quizZoneState.quizCount === quizZoneState.currentQuiz?.currentIndex ||
+                stage == 'RESULT';
+            return <QuizCompleted isLastQuiz={isLastQuiz} />;
         default:
             return null;
     }
