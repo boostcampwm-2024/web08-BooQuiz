3039d69d673bdfae39355df2b4cb70e53b0702c2
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-11-25 02:27:20 +0900

    feat: 퀴즈, 퀴즈셋 엔티티 추가
    Story: #120

---
+++ apps/backend/src/quiz/quiz.entitiy.ts
@@ -0,0 +1,47 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { QuizSet } from './quiz-set.entity';
+import { QUIZ_TYPE } from '../common/constants';
+
+/**
+ * 퀴즈 엔티티
+ *
+ * @property question: 퀴즈의 질문
+ * @property answer: 퀴즈의 정답
+ * @property playTime: 퀴즈의 플레이 시간
+ */
+@Entity()
+export class Quiz {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    question: string;
+
+    @Column()
+    answer: string;
+
+    @Column()
+    playTime: number;
+
+    @Column({ name: 'quiz_type' })
+    quizType: string;
+
+    @ManyToOne((type) => QuizSet, (quizSet) => quizSet.quizzes, {
+        onDelete: 'CASCADE',
+    })
+    quizSet: QuizSet;
+
+    constructor(
+        question: string,
+        answer: string,
+        playTime: number,
+        quizType: QUIZ_TYPE,
+        quizSet: QuizSet,
+    ) {
+        this.question = question;
+        this.answer = answer;
+        this.playTime = playTime;
+        this.quizType = quizType;
+        this.quizSet = quizSet;
+    }
+}
