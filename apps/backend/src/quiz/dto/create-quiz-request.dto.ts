17e18fe2fe07fe914344e75adcd8f59987d955e1
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-11-27 15:24:41 +0900

    refactor: dto 수정
    Story: #120

--- apps/backend/src/quiz/dto/create-quiz-request.dto.ts
+++ apps/backend/src/quiz/dto/create-quiz-request.dto.ts
@@ -2,25 +2,28 @@ import { ApiProperty } from '@nestjs/swagger';
 import { QUIZ_TYPE } from '../../common/constants';
 import { Quiz } from '../entity/quiz.entitiy';
 import { QuizSet } from '../entity/quiz-set.entity';
-import { IsEnum, IsNumber, IsString, Length, ValidateNested } from 'class-validator';
+import { IsEnum, IsNumber, IsString, Length, Max, Min, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class QuizDetailsDto {
     @ApiProperty({ description: '퀴즈 질문' })
-    @IsString()
-    @Length(1, 255)
+    @IsString({message: '퀴즈 질문은 문자열이어야 합니다.'})
+    @Length(1, 200, {message: '퀴즈의 질문은 1~200자 이어야 합니다.'})
     readonly question: string;
 
     @ApiProperty({ description: '퀴즈 정답' })
-    @IsString()
+    @IsString({message: '퀴즈 정답은 문자열이어야 합니다.'})
+    @Length(1, 30, {message: '퀴즈의 대답은 1~30자 이어야 합니다.'})
     readonly answer: string;
 
     @ApiProperty({ description: '퀴즈 시간' })
-    @IsNumber()
+    @Min(3, {message: '퀴즈 시간은 3초 이상이어야 합니다.'})
+    @Max(60 * 10, {message: '퀴즈 시간은 10분 이하여야 합니다.'})
+    @IsNumber({}, {message: '퀴즈 시간 값이 숫자가 아닙니다.'})
     readonly playTime: number;
 
     @ApiProperty({ description: '퀴즈 타입' })
-    @IsEnum(QUIZ_TYPE)
+    @IsEnum(QUIZ_TYPE, {message: '정해진 퀴즈 타입이 아닙니다.'})
     readonly quizType: QUIZ_TYPE;
 
     toEntity(quizSet: QuizSet) {
@@ -31,6 +34,8 @@ export class QuizDetailsDto {
 export class CreateQuizRequestDto {
 
     @ApiProperty()
+    @IsString({message: '퀴즈셋의 이름은 문자열이어야 합니다.'})
+    @Length(1, 30, {message: '퀴즈셋의 이름은 1~30자 이어야 합니다.'})
     readonly quizSetName: string;
 
     @ApiProperty({ type: [QuizDetailsDto], description: '퀴즈 세부 정보' })
