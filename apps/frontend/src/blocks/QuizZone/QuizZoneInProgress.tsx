01e62cab885c03798b5d65674cd08213c2c5c574
krokerdile <crazykroker@gmail.com>
2024-11-28 03:31:11 +0900

    refactor: QuizZoneInProgress 컴포넌트에서 채팅 기능 제거
    - QuizZoneInProgress 컴포넌트의 props에서 sendChat, chatMessages 관련 속성 제거
    - QuizWaiting, QuizCompleted 컴포넌트에서 채팅 관련 props 전달 제거
    - currentPlayer에서 사용하지 않는 id, nickname 속성 참조 제거
    
    Story: #123

--- apps/frontend/src/blocks/QuizZone/QuizZoneInProgress.tsx
+++ apps/frontend/src/blocks/QuizZone/QuizZoneInProgress.tsx
@@ -2,35 +2,24 @@ import { QuizZone } from '@/types/quizZone.types';
 import QuizWaiting from './QuizWaiting';
 import QuizInProgress from './QuizInProgress';
 import QuizCompleted from './QuizCompleted';
-import { ChatMessage } from '@/types/quizZone.types';
 
 interface QuizZoneInProgressProps {
     quizZoneState: QuizZone;
-    sendChat: (chatMessage: ChatMessage) => void;
     submitAnswer: (answer: string) => void;
     playQuiz: () => void;
 }
 
-const QuizZoneInProgress = ({
-    quizZoneState,
-    sendChat,
-    submitAnswer,
-    playQuiz,
-}: QuizZoneInProgressProps) => {
-    const { currentPlayer, currentQuiz, chatMessages, currentQuizResult } = quizZoneState;
-    const { id, nickname, state } = currentPlayer;
+const QuizZoneInProgress = ({ quizZoneState, submitAnswer, playQuiz }: QuizZoneInProgressProps) => {
+    const { currentPlayer, currentQuiz, currentQuizResult } = quizZoneState;
+    const { state } = currentPlayer;
     const { playTime, startTime } = currentQuiz ?? {};
 
     switch (state) {
         case 'WAIT':
             return (
                 <QuizWaiting
-                    id={id}
-                    nickname={nickname}
-                    chatMessages={chatMessages ?? []}
                     startTime={startTime!}
                     playQuiz={playQuiz}
-                    sendChat={sendChat}
                     currentQuizSummary={currentQuizResult}
                 />
             );
@@ -45,10 +34,6 @@ const QuizZoneInProgress = ({
         case 'SUBMIT':
             return (
                 <QuizCompleted
-                    id={id}
-                    nickname={nickname}
-                    chatMessages={chatMessages ?? []}
-                    sendChat={sendChat}
                     isLastQuiz={quizZoneState.isLastQuiz ?? false}
                     deadlineTime={quizZoneState.currentQuiz?.deadlineTime ?? 0}
                     currentQuizResult={quizZoneState.currentQuizResult!}
