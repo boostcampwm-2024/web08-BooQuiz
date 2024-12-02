f6dc4089325764c1a6e2d2f564baa14ec7eeea63
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-12-02 17:18:55 +0900

    feat: 검색 시 검색 내용이 없으면 recommend인 퀴즈셋 반환
    ISSUE: #209

--- apps/backend/src/quiz/dto/search-quiz-set-request.dto.ts
+++ apps/backend/src/quiz/dto/search-quiz-set-request.dto.ts
@@ -4,7 +4,7 @@ import { IsNumber, IsString, Length } from 'class-validator';
 export class SearchQuizSetRequestDTO {
     @IsString({ message: '퀴즈셋의 이름은 문자열이어야 합니다.' })
     @Length(0, 30, { message: '퀴즈셋의 이름은 1~30자 이어야 합니다.' })
-    readonly name: string;
+    readonly name: string = '';
     @Type(() => Number)
     @IsNumber({}, { message: 'page 값이 숫자가 아닙니다.' })
     readonly page?: number = 1;
