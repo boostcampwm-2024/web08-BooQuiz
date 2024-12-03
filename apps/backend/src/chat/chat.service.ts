3ff8fb3f0d0a7f066d1f13c29beca401284d4132
선빈 <ehtjsqls@gmail.com>
2024-12-03 14:52:27 +0900

    feat: 채팅 서비스에 대한 테스트 추가 및 예외 처리 로직 개선
    Story: #222

--- apps/backend/src/chat/chat.service.ts
+++ apps/backend/src/chat/chat.service.ts
@@ -14,14 +14,14 @@ export class ChatService {
     }
 
     async get(id: string) {
-        if (!this.chatRepository.has(id)) {
+        if (!(await this.chatRepository.has(id))) {
             throw new NotFoundException('퀴즈 존에 대한 채팅이 존재하지 않습니다.');
         }
         return this.chatRepository.get(id);
     }
 
     async add(id: string, chatMessage: ChatMessage) {
-        if (!this.chatRepository.has(id)) {
+        if (!(await this.chatRepository.has(id))) {
             throw new NotFoundException('퀴즈 존에 대한 채팅이 존재하지 않습니다.');
         }
         return this.chatRepository.add(id, chatMessage);
