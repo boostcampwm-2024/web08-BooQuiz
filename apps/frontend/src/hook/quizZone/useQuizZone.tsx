cd5af58786bc2be77de3752127f1fb1b198016ef
krokerdile <crazykroker@gmail.com>
2024-12-03 10:10:12 +0900

    fix: QuizZoneResult 결과 페이지 타이머 처리 추가, useQuizZone lint 처리

--- apps/frontend/src/hook/quizZone/useQuizZone.tsx
+++ apps/frontend/src/hook/quizZone/useQuizZone.tsx
@@ -159,7 +159,7 @@ const quizZoneReducer: Reducer<QuizZone, QuizZoneAction> = (state, action) => {
                 submits: payload.submits,
                 quizzes: payload.quizzes,
                 ranks: payload.ranks,
-                endSocketTime: payload.endSocketTime
+                endSocketTime: payload.endSocketTime,
             };
         case 'chat':
             return {
