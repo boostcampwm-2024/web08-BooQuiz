caff760a83fa9818bd72312244154e153105f6df
JunhyunKang <kangjunhyun38@gmail.com>
2024-12-05 10:03:29 +0900

    test: 부하테스트 grafana,influxdb

--- apps/backend/k6script.js
+++ apps/backend/k6script.js
@@ -1,44 +1,43 @@
 import http from 'k6/http';
 import ws from 'k6/ws';
 import { check, sleep } from 'k6';
+// 첫 번째 방 시나리오
 
-// HTTP 요청 후 WebSocket 연결하는 통합 테스트
-export default function () {
-    // 세션 ID 생성
-    const httpResponse = http.get('http://test.booquiz.kro.kr/api/quiz-zone/testtest');
+export function scenario1() {
+    const httpResponse = http.get('https://booquiz.kro.kr/api/quiz-zone/loadtest');
 
-    // 2. 응답 헤더에서 Set-Cookie를 확인합니다
+    check(httpResponse, {
+        'is status 200': (r) => r.status === 200,
+        'is status 400': (r) => r.status === 400,
+        'is status 500': (r) => r.status === 500,
+    });
+
     const cookies = httpResponse.headers['Set-Cookie'];
     const sessionCookie = cookies ? cookies.split(';')[0] : '';
-    // console.log('Received session cookie:', sessionCookie);
 
     sleep(1);
 
-    // 3. 받은 쿠키를 그대로 WebSocket 연결에 사용합니다
     const wsParams = {
         headers: {
             Cookie: sessionCookie,
         },
     };
-    // console.log(httpResponse.headers);
-    const wsResponse = ws.connect('ws://test.booquiz.kro.kr/api/play', wsParams, function (socket) {
+
+    const wsResponse = ws.connect('wss://booquiz.kro.kr/api/play', wsParams, function (socket) {
         socket.on('open', () => {
             sleep(2);
             socket.send(
                 JSON.stringify({
                     event: 'join',
-                    data: { quizZoneId: 'testtest' },
+                    data: { quizZoneId: 'loadtest' },
                 }),
             );
         });
 
         socket.on('message', (data) => {
             data = JSON.parse(data);
-            console.log(data.event);
             if (data.event === 'someone_join') {
                 sleep(2);
-                // console.log(data);
-
                 socket.send(
                     JSON.stringify({
                         event: 'chat',
@@ -54,17 +53,132 @@ export default function () {
     });
 
     check(wsResponse, {
-        'WebSocket connected successfully': (r) => r && r.status === 101,
+        'WebSocket connected successfully (Room 1)': (r) => r && r.status === 101,
     });
 }
+
+// 두 번째 방 시나리오
+export function scenario2() {
+    const httpResponse = http.get('https://booquiz.kro.kr/api/quiz-zone/loadtest4');
+
+    check(httpResponse, {
+        'is status 200': (r) => r.status === 200,
+        'is status 400': (r) => r.status === 400,
+        'is status 500': (r) => r.status === 500,
+    });
+
+    const cookies = httpResponse.headers['Set-Cookie'];
+    const sessionCookie = cookies ? cookies.split(';')[0] : '';
+
+    sleep(1);
+
+    const wsParams = {
+        headers: {
+            Cookie: sessionCookie,
+        },
+    };
+
+    const wsResponse = ws.connect('wss://booquiz.kro.kr/api/play', wsParams, function (socket) {
+        socket.on('open', () => {
+            sleep(2);
+            socket.send(
+                JSON.stringify({
+                    event: 'join',
+                    data: { quizZoneId: 'loadtest4' },
+                }),
+            );
+        });
+
+        socket.on('message', (data) => {
+            data = JSON.parse(data);
+            if (data.event === 'someone_join') {
+                sleep(2);
+                socket.send(
+                    JSON.stringify({
+                        event: 'chat',
+                        data: {
+                            clientId: wsParams.headers.Cookie,
+                            nickname: `${data.data.nickname}`,
+                            message: `안녕 나는 ${data.data.nickname}이야`,
+                        },
+                    }),
+                );
+            }
+        });
+    });
+
+    check(wsResponse, {
+        'WebSocket connected successfully (Room 2)': (r) => r && r.status === 101,
+    });
+}
+//
+// export function scenario3() {
+//     const httpResponse = http.get('https://booquiz.kro.kr/api/quiz-zone/loadtest3');
+//     const cookies = httpResponse.headers['Set-Cookie'];
+//     const sessionCookie = cookies ? cookies.split(';')[0] : '';
+//
+//     sleep(1);
+//
+//     const wsParams = {
+//         headers: {
+//             Cookie: sessionCookie,
+//         },
+//     };
+//
+//     const wsResponse = ws.connect('wss://booquiz.kro.kr/api/play', wsParams, function (socket) {
+//         socket.on('open', () => {
+//             sleep(2);
+//             socket.send(
+//                 JSON.stringify({
+//                     event: 'join',
+//                     data: { quizZoneId: 'loadtest3' },
+//                 }),
+//             );
+//         });
+//
+//         socket.on('message', (data) => {
+//             data = JSON.parse(data);
+//             if (data.event === 'someone_join') {
+//                 sleep(2);
+//                 socket.send(
+//                     JSON.stringify({
+//                         event: 'chat',
+//                         data: {
+//                             clientId: wsParams.headers.Cookie,
+//                             nickname: `${data.data.nickname}`,
+//                             message: `안녕 나는 ${data.data.nickname}이야`,
+//                         },
+//                     }),
+//                 );
+//             }
+//         });
+//     });
+//
+//     check(wsResponse, {
+//         'WebSocket connected successfully (Room 2)': (r) => r && r.status === 101,
+//     });
+// }
 
 // 테스트 설정
 export const options = {
     scenarios: {
-        contacts: {
+        room1: {
             executor: 'ramping-vus',
+            exec: 'scenario1', // 첫 번째 시나리오 실행
             startVUs: 0,
-            stages: [{ duration: '3s', target: 5 }],
+            stages: [{ duration: '1s', target: 300 }],
         },
+        room2: {
+            executor: 'ramping-vus',
+            exec: 'scenario2', // 두 번째 시나리오 실행
+            startVUs: 0,
+            stages: [{ duration: '1s', target: 300 }],
+        },
+        // room3: {
+        //     executor: 'ramping-vus',
+        //     exec: 'scenario3', // 두 번째 시나리오 실행
+        //     startVUs: 0,
+        //     stages: [{ duration: '1s', target: 300 }],
+        // },
     },
 };
